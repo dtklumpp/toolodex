aa18d5910eab0d2b4ffbab48754e4770c0e06e5c
dtklumpp <dtklumpp@gmail.com>
2020-09-17 12:48:55 -0400

    server set up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+app.use(express.static('public'));
+//const path = require('path');
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({extended: false}));
+
+app.listen(PORT, () => {
+    console.log('greetings your port is '+PORT);
+})
+
+app.get('/', (req,res) => {
+    res.send('you have reached the home folder');
+})
+
