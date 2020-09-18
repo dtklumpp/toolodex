a19c74efdfc5d4f545210af53203fc6f3e2dffdd
dylanaldrich <aldrich.dylan@gmail.com>
2020-09-18 15:55:36 -0400

    Requires new packages and sets up new controller routes in server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+/* External Modules */
 const express = require('express');
-const app = express();
-const PORT = 4000;
-
 const methodOverride = require('method-override');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+/* Internal Modules */
+const db = require('./models');
+const controllers = require('./controllers');
+
+/* Instantiated Modules */
+const app = express();
+
+/* Configuration */
+const PORT = 4000;
+app.set('view engine', 'ejs');
+
+/* Middleware */
 app.use(methodOverride('_method'));
 
 app.use(express.static('public'));
@@ -10,13 +23,19 @@ app.use(express.static('public'));
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 
-const db = require('./models');
-const controllers = require('./controllers');
-
+/* Routes */
+// index view
 app.get('/', (req,res) => {
-    res.send('you have reached the home folder');
+    res.send('You have reached the homepage.');
 });
 
+// Category routes
+app.use('/categories', controllers.category_control);
+
+// Tool routes
+app.use('/tools', controllers.tool_control);
+
+/* Server Listener */
 app.listen(PORT, () => {
     console.log('greetings your port is '+PORT);
 });
