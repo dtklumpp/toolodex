e5d02b55a9ab004b6f62afaf93390a557216c31e
dtklumpp <dtklumpp@gmail.com>
2020-09-18 20:13:35 -0400

    made tool model and New page

--- controllers/tool_control.js
+++ controllers/tool_control.js
@@ -1,3 +1,22 @@
 const express = require('express')
 const router = express.Router();
 module.exports = router;
+
+const db = require('../models');
+
+//index route
+router.get('/', (req, res) => {
+    res.send('tools index page');
+})
+
+//new route
+router.get('/newTool', (req, res) => {
+    res.render('/tool/new.ejs');
+})
+
+
+//update route
+//show route
+//edit route
+//create route
+//delete route
