918815d4583aaeb5cd21b3979f101696bc77fd58
dylanaldrich <aldrich.dylan@gmail.com>
2020-09-25 14:31:33 -0400

    Adds script to capitalize username when found in H2 tags

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 console.log("Scripts are live on the browser.");
+
+/* Capitalize username when it appears in H2 tags */
+let variableHeading = document.querySelector('.variable-heading').innerText;
+
+let capitalizedHeading = null;
+
+function capitalizeFirstLetter(string) {
+    return capitalizedHeading = string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+capitalizeFirstLetter(variableHeading); 
+
+document.querySelector('.variable-heading').innerText = capitalizedHeading;
