b4dc76a9bfa6f43963309fc4e9d5f8735866092b
dylanaldrich <aldrich.dylan@gmail.com>
2020-09-25 10:43:31 -0400

    Cleans up user controller

--- controllers/user_control.js
+++ controllers/user_control.js
@@ -7,40 +7,21 @@ const db = require('../models');
 const statesUSA = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'United Arab Emirates', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'XX' ];
 
 
-//index route
+// index route
 router.get('/', async (req, res) => {
-    //res.send('tools index page');
     try{
         const usersArray = await db.User.find({});
         context = {allUsers: usersArray};
         res.render('user/index.ejs', context);
     }
-    catch(error){
-        res.send("index route error: "+error);
+    catch(error) {
+        console.log('Error: ', error);
+        return res.send('Internal server error.');
     }
-})
+});
 
 
-
-//REPLACED BY REGISTER FORM
-//new route
-// router.get('/newUser', (req, res) => {
-//     res.render('user/new.ejs', {statesArray: statesUSA});
-// })
-
-//REPLACED BY REGISTER POST
-//create route
-// router.post('/', (req, res) => {
-//     //console.log('got here');
-//     db.User.create(req.body, (err, createdUser) => {
-//         if(err) return res.send('create route error: '+err);
-//         res.redirect('/users');
-//     })
-// })
-
-
-
-//show route
+// show route
 router.get('/:userId', async (req, res) => {
     try{
         const foundUser = await db.User.findById(req.params.userId)
@@ -52,7 +33,7 @@ router.get('/:userId', async (req, res) => {
         })
         .populate('friends')
         .exec();
-        //console.log('foundUser.friends:', foundUser.friends)
+
         const context = {
             oneUser: foundUser,
             userCats: foundUser.categories,
@@ -61,13 +42,14 @@ router.get('/:userId', async (req, res) => {
         };
         res.render('user/show.ejs', context);
     }
-    catch(error){
-        res.send('show route error: '+error);
+    catch(error) {
+        console.log('Error: ', error);
+        return res.send('Internal server error.');
     }
-})
+});
 
 
-//edit route
+// edit route
 router.get('/:userId/edit', async (req, res) => {
     try{
         const foundUser = await db.User.findById(req.params.userId);
@@ -78,17 +60,14 @@ router.get('/:userId/edit', async (req, res) => {
         res.render('user/edit.ejs', context);
     }
     catch(error){
-        res.send('edit route error: '+error)
+        console.log('Error: ', error);
+        return res.send('Internal server error.');
     }
-})
+});
 
 
-
-
-
-//update route
+// update route
 router.put('/:userId', async (req, res) => {
-    //console.log('got hereabouts');
     try{
         const updatedUser = await db.User.findByIdAndUpdate(req.params.userId, req.body, {new: true});
         res.redirect('/users/'+req.params.userId);
@@ -99,10 +78,10 @@ router.put('/:userId', async (req, res) => {
 })
 
 
-
-//delete route
+// delete route
 router.delete('/:userId', async (req, res) => {
     try{
+        // find user to be deleted, populate their categories and tools
         const doomedUser = await db.User.findById(req.params.userId)
         .populate({
             path: 'categories',
@@ -113,25 +92,31 @@ router.delete('/:userId', async (req, res) => {
         .populate('friends')
         .exec();
 
+        // remove categories from user (and from DB)
         const childCats = doomedUser.categories;
         for(eachCat of childCats){
             eachCat.user = null;
             eachCat.save();
         }
 
+        // remove user's friends
         const lostFriends = doomedUser.friends;
         for(eachFriend of lostFriends){
             eachFriend.friends.remove(doomedUser);
             eachFriend.save();
         }
 
+        // delete user
         doomedUser.deleteOne();
-        res.redirect('/users');
+
+        //redirect to homepage
+        res.redirect('/');
     }
     catch(error){
-        res.send('delete route error: '+error)
+        console.log('Error: ', error);
+        return res.send('Internal server error.');
     }
-})
+});
 
 //IMPORTANT:
 // THIS ISNT TESTED SO MAYBE LEAVE OFF TIL AFTER DEMO DAY?
